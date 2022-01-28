3bc29f33f3805a895e6d1d5afd18526caeed17e4
Matthew Macdonald-Wallace <matt@doics.co>
2022-01-28 08:43:34 +0000

    Working signups and logins

---
+++ membership/models.py
@@ -0,0 +1,48 @@
+from django.utils import timezone
+from django.db import models
+from django.conf import settings
+# Create your models here.
+MEMBERSHIP_CHOICES = (
+('Core', 'core'),
+('Full', 'full'),
+('Part Time', 'part-time')
+)
+
+class Membership(models.Model):
+    slug = models.SlugField(null=True, blank=True)
+    membership_type = models.CharField(
+    choices=MEMBERSHIP_CHOICES, default='Full',
+    max_length=30
+      )
+    price = models.DecimalField(default=0,
+            decimal_places=2,
+            max_digits=5)
+    description = models.TextField(default='')
+    stripe_price_id = models.CharField(default='', max_length=100)
+
+    def __str__(self):
+       return self.membership_type
+
+class UserMembership(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL,
+            related_name='user_membership',
+            on_delete=models.CASCADE)
+    membership = models.ForeignKey(Membership, 
+            related_name='user_membership', 
+            on_delete=models.SET_NULL, 
+            null=True)
+    stripe_customer_id = models.CharField(default='', max_length=255)
+    start_date = models.DateTimeField(auto_now_add=True)
+    end_date = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+       return self.user.username
+
+class Subscription(models.Model):
+    user_membership = models.ForeignKey(UserMembership,
+            related_name='subscription',
+            on_delete=models.CASCADE)
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+      return self.user_membership.user.username
---
+++ membership/urls.py
@@ -0,0 +1,15 @@
+### urls.py
+from django.urls import path, include 
+
+from . import views
+
+app_name = 'membership'
+
+urlpatterns = [
+       path('', views.MembershipView.as_view(), name='select'),
+       path('config/', views.stripe_config),
+       path('create-checkout-session/', views.create_checkout_session), # new
+       path('success/', views.SuccessView.as_view()), # new
+       path('cancelled/', views.CancelledView.as_view()), # new
+       path('webhook/', views.stripe_webhook), # new
+]
