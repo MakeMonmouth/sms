d1b3b9b3c1c7fd2b9d158287574c537ca7cf1c51
Matthew Macdonald-Wallace <matt@doics.co>
2022-01-30 08:53:10 +0000

    Trying to find the static assets

--- sms/settings.py
+++ sms/settings.py
@@ -118,8 +118,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
-STATIC_URL = 'assets/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticassets')
+STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
